739ebf77ce473a1ecf841e837b2d5deeb656c725
Wadser12346 <lam.ho155@gmail.com>
2020-03-09 13:06:40 -0700

    Added JAR File

--- src/MainApplication/Main.java
+++ src/MainApplication/Main.java
@@ -1,7 +1,9 @@
 package MainApplication;
 
+import MainApplication.Controller.ClientController;
 import MessageBusFiles.*;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +13,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Thread clientChat = new Thread(new ClientChatroom());
-        clientChat.start();
+
+        FXMLLoader chatTabs = new FXMLLoader(getClass().getResource("src/MainApplication/FXML/Client.fxml"));
+        chatTabs.setController(new ClientController());
+        primaryStage.setTitle("Message Bus");
+
+
+//        Thread clientChat = new Thread(new ClientChatroom());
+//        clientChat.start();
     }
 }
