de9af510b1a207e24d0dd8fc8467efcea120f2a6
Wadser12346 <lam.ho155@gmail.com>
2020-03-09 13:37:37 -0700

    UI loading doesnt work. Check Main.java line 21

--- src/MainApplication/Controller/ClientController.java
+++ src/MainApplication/Controller/ClientController.java
@@ -2,4 +2,8 @@ package MainApplication.Controller;
 import javafx.fxml.FXML;
 
 public class ClientController {
+      public void initalize(){
+          //Creates one tab with blank chatroom template
+
+      }
 }
--- src/MainApplication/Main.java
+++ src/MainApplication/Main.java
@@ -4,6 +4,8 @@ import MainApplication.Controller.ClientController;
 import MessageBusFiles.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,12 +16,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-//        FXMLLoader chatTabs = new FXMLLoader(getClass().getResource("src/MainApplication/FXML/Client.fxml"));
-//        chatTabs.setController(new ClientController());
-//        primaryStage.setTitle("Message Bus");
+        FXMLLoader chatTabs = new FXMLLoader(getClass().getResource("src/MainApplication/FXML/Client.fxml"));
+        chatTabs.setController(new ClientController());
+        Parent mainClient = chatTabs.load();
+        primaryStage.setTitle("Message Bus");
+        primaryStage.setScene(new Scene(mainClient, 600, 400));
+        primaryStage.show();
 
-
-        Thread clientChat = new Thread(new ClientChatroom());
-        clientChat.start();
+//        Thread clientChat = new Thread(new ClientChatroom());
+//        clientChat.start();
     }
 }
